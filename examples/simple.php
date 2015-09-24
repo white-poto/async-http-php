b604a5af7c91176494d3eae4855d489d05e560d9
huyanping <hypxm@qq.com>
2015-09-24 14:29:07 +0800

    remove callback

--- examples/simple.php
+++ examples/simple.php
@@ -9,19 +9,23 @@
 date_default_timezone_set("PRC");
 require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-echo '1' . PHP_EOL;
 $async = new \Jenner\Http\Async();
 $task = \Jenner\Http\Task::createGet("http://www.baidu.com");
 $async->attach($task, "baidu");
 
-echo '2' . PHP_EOL;
 $task2 = \Jenner\Http\Task::createGet("http://www.sina.com");
 $async->attach($task2, "sina");
 
-echo '3' . PHP_EOL;
 $task3 = \Jenner\Http\Task::createGet("http://www.qq.com");
 $async->attach($task3, "qq");
 
-echo "4" . PHP_EOL;
-$result = $async->execute();
-print_r($result);
+while(true){
+    if(!$async->isDone()){
+        sleep(1);
+        continue;
+    }
+
+    $result = $async->execute();
+    print_r($result);
+}
+
